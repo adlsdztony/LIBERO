19f2a164f848e4c191ee1d388ee2cd06ce13c9d7
Zhen Qi <152520300+KellyYankei@users.noreply.github.com>
2025-06-13 22:49:31 -0400

    Add tasks
    Add task "Place the chocolate pudding in the wooden tray with its orientation rotated 90 degrees from upright"
    Add task "Put the salad dressing bottle in one bowl and then place the bowl in the wooden tray"

---
+++ tasks/LIVING_ROOM_SCENE4_put_the_salad_dressing_bottle_in_one_bowl_and_then_place_the_bowl_in_the_wooden_tray.py
@@ -0,0 +1,43 @@
+"""This is a standalone file for create a task in libero."""
+import numpy as np
+
+from libero.libero.utils.bddl_generation_utils import (
+    get_xy_region_kwargs_list_from_regions_info,
+)
+from libero.libero.utils.mu_utils import register_mu, InitialSceneTemplates
+from libero.libero.utils.task_generation_utils import (
+    register_task_info,
+    generate_bddl_from_task_info,
+    get_task_info
+)
+
+from libero.libero.benchmark.mu_creation import *
+
+
+def main():
+    # living_room_scene_4
+    scene_name = "living_room_scene4"
+    language = "Put the salad dressing bottle in one bowl and then place the bowl in the wooden tray"
+    register_task_info(
+        language,
+        scene_name=scene_name,
+        objects_of_interest=["new_salad_dressing_1", "wooden_tray_1","akita_black_bowl_1","akita_black_bowl_2"],
+        goal_states=[
+            ('Or',
+                ('And',
+           ('On', 'new_salad_dressing_1', 'akita_black_bowl_1'), 
+           ('In', 'akita_black_bowl_1', 'wooden_tray_1_contain_region')),
+           ('And',
+            ('On','new_salad_dressing_1', 'akita_black_bowl_2'), 
+            ('In', 'akita_black_bowl_2', 'wooden_tray_1_contain_region'))
+       ),  
+        ],
+    )
+
+
+    bddl_file_names, failures = generate_bddl_from_task_info()
+    print(bddl_file_names)
+
+
+if __name__ == "__main__":
+    main()
