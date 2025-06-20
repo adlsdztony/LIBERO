af2734a89868895f90474d43ca1ec53085af6cab
Junhao Chen <chenjunh22@mails.tsinghua.edu.cn>
2025-06-20 15:20:35 +0800

    Dev: Implemented 5 kitchen_scene_7 tasks

---
+++ tasks/KITCHEN_SCENE7_hold_the_bowl_in_the_air_with_its_opening_facing_sideways.py
@@ -0,0 +1,95 @@
+import numpy as np
+
+from libero.libero.utils.bddl_generation_utils import (
+    get_xy_region_kwargs_list_from_regions_info,
+)
+from libero.libero.utils.mu_utils import register_mu, InitialSceneTemplates
+from libero.libero.utils.task_generation_utils import (
+    register_task_info,
+    get_task_info,
+    generate_bddl_from_task_info,
+)
+
+
+@register_mu(scene_type="kitchen")
+class KitchenScene7(InitialSceneTemplates):
+    def __init__(self):
+
+        fixture_num_info = {
+            "kitchen_table": 1,
+            "microwave": 1,
+        }
+
+        object_num_info = {
+            "white_bowl": 1,
+            "plate": 1,
+        }
+
+        super().__init__(
+            workspace_name="kitchen_table",
+            fixture_num_info=fixture_num_info,
+            object_num_info=object_num_info,
+        )
+
+    def define_regions(self):
+        self.regions.update(
+            self.get_region_dict(
+                region_centroid_xy=[0.0, -0.25],
+                region_name="microwave_init_region",
+                target_name=self.workspace_name,
+                region_half_len=0.01,
+                yaw_rotation=(np.pi, np.pi),
+            )
+        )
+
+        self.regions.update(
+            self.get_region_dict(
+                region_centroid_xy=[0.0, 0.0],
+                region_name="plate_init_region",
+                target_name=self.workspace_name,
+                region_half_len=0.025,
+            )
+        )
+
+        self.regions.update(
+            self.get_region_dict(
+                region_centroid_xy=[0.0, 0.10],
+                region_name="plate_right_region",
+                target_name=self.workspace_name,
+                region_half_len=0.05,
+            )
+        )
+
+        self.xy_region_kwargs_list = get_xy_region_kwargs_list_from_regions_info(
+            self.regions
+        )
+
+    @property
+    def init_states(self):
+        states = [
+            ("On", "white_bowl_1", "microwave_1_top_side"),
+            ("On", "microwave_1", "kitchen_table_microwave_init_region"),
+            ("Close", "microwave_1"),
+            ("On", "plate_1", "kitchen_table_plate_init_region"),
+        ]
+        return states
+
+
+def main():
+    scene_name = "kitchen_scene7"
+    language = "Hold the bowl in the air with its opening facing sideways"
+    register_task_info(
+        language,
+        scene_name=scene_name,
+        objects_of_interest=["white_bowl_1", "microwave_1", "plate_1"],
+        goal_states=[
+            ("AxisAlignedWithinWorldAxis", "white_bowl_1", "z", 85, 95, "z"),
+            ("InAir", "white_bowl_1", 0.5),
+        ],
+    )
+    bddl_file_names, failures = generate_bddl_from_task_info()
+    print(bddl_file_names)
+
+
+if __name__ == "__main__":
+    main()
