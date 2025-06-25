6f428f538a264679df822efd98510757a46e212c
Qirui Chen <qirui@QiruideMacBook-Air.local>
2025-06-25 19:42:13 +0800

    Add 2 tasks to living room scene 1

---
+++ tasks/LIVING_ROOM_SCENE1_lay_the_alpha_soup_bottle_flat_touching_the_basket.py
@@ -0,0 +1,40 @@
+"""This is a standalone file for create a task in libero."""
+import numpy as np
+
+from libero.libero.utils.bddl_generation_utils import (
+    get_xy_region_kwargs_list_from_regions_info,
+)
+from libero.libero.utils.mu_utils import register_mu, InitialSceneTemplates
+from libero.libero.utils.task_generation_utils import (
+    register_task_info,
+    get_task_info,
+    generate_bddl_from_task_info,
+)
+
+from libero.libero.benchmark.mu_creation import *
+
+def main():
+
+    scene_name = "living_room_scene1"
+    language = "Lay the alpha soup bottle flat touching the basket"
+    # print(scene_name.upper() + '_' + '_'.join(language.lower().split()) + '.py')
+    register_task_info(
+        language,
+        scene_name=scene_name,
+        objects_of_interest=["ketchup_1", "basket_1"],
+        goal_states=[
+            ("InContact", "ketchup_1", "basket_1"),
+            (
+                "Or",
+                ("AxisAlignedWithin", "ketchup_1", "z", 0, 20),
+                ("AxisAlignedWithin", "ketchup_1", "z", 160, 180)
+            ),
+        ]
+    )
+
+    bddl_file_names, failures = generate_bddl_from_task_info()
+    print(bddl_file_names)
+
+
+if __name__ == "__main__":
+    main()
