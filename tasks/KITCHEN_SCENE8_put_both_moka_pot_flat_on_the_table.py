c52342ddc0a59cc2f98a250beabb43b1acafb5e3
Junhao Chen <chenjunh22@mails.tsinghua.edu.cn>
2025-06-26 16:45:30 +0800

    Dev: Implemented KITCHEN_SCENE8_put_both_moka_pot_flat_on_the_table

---
+++ tasks/KITCHEN_SCENE8_put_both_moka_pot_flat_on_the_table.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from libero.libero.utils.bddl_generation_utils import (
+    get_xy_region_kwargs_list_from_regions_info,
+)
+from libero.libero.utils.mu_utils import register_mu, InitialSceneTemplates
+from libero.libero.utils.task_generation_utils import (
+    register_task_info,
+    get_task_info,
+    generate_bddl_from_task_info,
+)
+from libero.libero.benchmark.mu_creation import *
+
+def main():
+    scene_name = "kitchen_scene8"
+    language = "Put both moka pot flat on the table"
+    register_task_info(
+        language,
+        scene_name=scene_name,
+        objects_of_interest=["moka_pot_1", "moka_pot_2", "flat_stove_1"],
+        goal_states=[
+            ("AxisAlignedWithinWorldAxis", "moka_pot_1", "z", 85, 95, "z"),
+            ("AxisAlignedWithinWorldAxis", "moka_pot_2", "z", 85, 95, "z"),
+        ],
+    )
+    bddl_file_names, failures = generate_bddl_from_task_info()
+    print(bddl_file_names)
+
+if __name__ == "__main__":
+    main()
